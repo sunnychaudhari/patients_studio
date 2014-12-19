7f82d6870e1446fa3ad288977b55e9800ad13b1a
sunnychaudhari <yuvaraj.chaudhari1@gmail.com>
2014-12-19 20:24:40 +0530

    added DB structure and required fields to store patients information

--- p_stats/p_app/models.py
+++ p_stats/p_app/models.py
@@ -1,13 +1,56 @@
+# imports from python libraries 
+import os, datetime
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from django_mongokit import connection
+from django_mongokit import get_database
+from django_mongokit.document import DjangoDocument
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+#from mongokit import CustomType
+from mongokit import IS
+#from mongokit import OR
 
+try:
+    from bson import ObjectId
+except ImportError:  # old pymongo
+    from pymongo.objectid import ObjectId
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+##################################################
+
+@connection.register
+class Patient(DjangoDocument):
+    '''This is a 'Patient' collection where all its information will be 
+       recorded for check up in hospital
+    '''
+    objects = models.Manager()
+
+    collection_name = 'Patients'
+    structure = {
+        'Patient_ID': unicode, 
+        'f_name': unicode,
+        'l_name': unicode,
+        'address1': unicode,
+        'address2': unicode,
+        'city': unicode,
+        'phone': int,
+        'age': int,
+
+      	'created_at': datetime.datetime, # Patients entry 
+        'created_by': int, # test required: only ids of Users(hospital staff member)
+
+        'last_update': datetime.datetime, # Update entry
+        'modified_by': int, # test required: only ids of Users(hospital staff member)
+
+        'disease': unicode, 
+        'prescription': unicode, 
+
+        'collection_set': [ObjectId],  # for indivisual patients reports
+    }
+    
+    required_fields = ['f_name','l_name','address1','city','phone','age'] # 'group_set' to be included
+
+    default_values = {'created_at': datetime.datetime.utcnow}
+    use_dot_notation = True
+
