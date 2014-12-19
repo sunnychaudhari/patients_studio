335a958961850f7275cf12499973f0573f7e3660
sunnychaudhari <yuvaraj.chaudhari1@gmail.com>
2014-12-19 23:36:19 +0530

    url added for calling homepage method in views.py

--- p_stats/p_stats/urls.py
+++ p_stats/p_stats/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'p_stats.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'p_app.views.homepage', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
