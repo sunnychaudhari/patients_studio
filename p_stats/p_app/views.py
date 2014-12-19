b4be71ff3797efce909d4c037c4a66bd67120ac2
sunnychaudhari <yuvaraj.chaudhari1@gmail.com>
2014-12-19 23:37:13 +0530

    homepage() method for rendering the output on home.html template

--- p_stats/p_app/views.py
+++ p_stats/p_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+''' -- imports from installed packages -- '''
+from django.shortcuts import render_to_response, render
+from django.template import RequestContext
+from django_mongokit import get_database
+
+try:
+    from bson import ObjectId
+except ImportError:  # old pymongo
+    from pymongo.objectid import ObjectId
+
 
 # Create your views here.
+
+def homepage(request):	
+
+	return render_to_response("home.html",context_instance=RequestContext(request))
