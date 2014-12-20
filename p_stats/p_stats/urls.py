90ea0ccf2b0f21d99ff2984d2c93f4ea9213dfe5
sunnychaudhari <yuvaraj.chaudhari1@gmail.com>
2014-12-20 23:47:11 +0530

    url added to visit to details tab for showing he patients detail information

--- p_stats/p_stats/urls.py
+++ p_stats/p_stats/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'p_stats.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'p_app.views.homepage', name='home'),
+    url(r'^patient_details/(?P<p_id>[\w-]+)$', 'p_app.views.homepage', name='details'),
     url(r'^admin/', include(admin.site.urls)),
 )
